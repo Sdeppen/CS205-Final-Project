6ca4ad301158ff6aa7a32eb7aba81bd51ed0673b
Ting Zhou <tingzhou@college.harvard.edu>
2015-11-20 10:08:21 -0500

    edits for proper splitting

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ if __name__ == "__main__":
 
     sc = SparkContext()
     # Load and parse data into sections
-    DSM4 = sc.textFile("testfile.txt")
+    delimiter = "This page intentionally left blank\n"
+    contents = open("testfile.txt").read()
+    DSM4 = sc.parallelize(contents.split(delimiter))
+    # open / read the file
 
     #split words apart to be filtered
     #cleanup = DSM4.map(lambda w: w.split("This page intentionally left blank"))

ce39d92ea9376ce49e3feda7b596a49e733dc7c5
sdeppen <sdeppen@college.harvard.edu>
2015-11-28 17:58:34 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	main.py

--- main.py
+++ main.py
@@ -10,20 +10,26 @@ if __name__ == "__main__":
 
     sc = SparkContext()
     # Load and parse data into sections
+
     DSM4 = sc.textFile("dsm-iv-2edits.txt")
 
+    delimiter = "This page intentionally left blank\n"
+    contents = open("testfile.txt").read()
+    DSM4 = sc.parallelize(contents.split(delimiter))
+    # open / read the file
+
+   # delimiters = "This page intentionally left blank"
     #split words apart to be filtered
-    cleanup = DSM4.flatMap(lambda w: w.split("This page intentionally left blank")) #.map(lambda x: (x, 1)).reduceByKey(add)
+    cleanup = DSM4.flatMap(lambda w: w.split("\n"))#"This page intentionally left blank"))
     output = cleanup.collect()
 
     for (word) in output:
-        print("%s: END OF SECTION" % (word)) 
- 
-    #counts = DSM4.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(add)
+        print("%s \n \n" % (word))
+   
+   #counts = DSM4.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(add)
     #output = counts.collect()
     
    # for (word, count) in output:
-   #     print("%s: %i" % (word, count))
-
+   #     print("%s, %i: END OF LINE" % (word, count)) 
   
     sc.stop()

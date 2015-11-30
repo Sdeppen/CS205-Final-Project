206b1b650d6ccd07b27965a42639c2c4360d815e
sdeppen <sdeppen@college.harvard.edu>
2015-11-30 01:24:00 -0500

    Cleaned up a bit

--- main.py
+++ main.py
@@ -15,15 +15,13 @@ if __name__ == "__main__":
     delimiter = "This page intentionally left blank #"
     # contents = open("dsm-iv-2edits.txt").read()
     contents = open("testfile.txt").read()
-    DSM4 = sc.parallelize(contents.split("This page intentionally left blank #"))
+    DSM4 = sc.parallelize(contents.split(delimiter))
     # open / read the file
 
     # split words apart to be filtered
-    split = DSM4.flatMap(lambda x: x.split('#'))
+    split = DSM4.flatMap(lambda x: x.split(delimiter))
     output = split.collect()
 
-    # parsedData = DSM4.flatMap(lambda section:  section.split(delimiter))
-    
     print(split.take(1))
 
     #for (section) in output:
